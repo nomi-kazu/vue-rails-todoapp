b56e07f42627e72f19d7c58f05e6ddf2c7333de5
nomikazu <>
2021-07-02 20:02:20 +0900

    no message

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -4,7 +4,6 @@
     <a class="navbar-item" href="https://versions.bulma.io/0.7.0">
       <img src="https://versions.bulma.io/0.7.0/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
     </a>
-
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
