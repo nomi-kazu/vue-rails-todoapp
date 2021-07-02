bc4353d5d57f490901d1fa12cc5045c7012d56f9
nomikazu <>
2021-07-03 00:25:45 +0900

    fix: index todo

--- front/src/components/TodoList.vue
+++ front/src/components/TodoList.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="field is-grouped mt-6">
-    <p>
-      <input v-model="todo.title" class="input" type="text" placeholder="Add Todo">
-    </p>
-    <p class="control">
-      <a class="button is-link" @click="createTodo">
-        Add
-      </a>
-    </p>
-    <ul v-for="todo in todos" :key="todo.id">
-      <li>{{ todo.title }}</li>
-    </ul>
+  <div>
+    <div class="field is-grouped mt-6">
+      <p class="control is-expanded">
+        <input v-model="todo.title" class="input" type="text" placeholder="Add Todo">
+      </p>
+      <p class="control">
+        <a class="button is-link" @click="createTodo">
+          Add
+        </a>
+      </p>
+    </div>
+    <div class="content">
+      <ul v-for="todo in todos" :key="todo.id">
+        <li>{{ todo.title }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -39,7 +43,15 @@ export default {
       axios
         .post('http://localhost:5000/v1/todos', this.todo)
         .then(response => {
-          console.log('Created!');
+          this.updateTodos();
+          this.todo.title = '';
+        })
+    },
+    updateTodos: function() {
+      axios
+        .get('http://localhost:5000/v1/todos')
+        .then(response => {
+          this.todos = response.data;
         })
     }
   }
