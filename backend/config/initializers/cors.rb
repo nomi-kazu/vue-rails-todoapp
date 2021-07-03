1a019406264be595f906a60e8ec23c34d8c38f4b
nomikazu <>
2021-07-03 15:21:11 +0900

    fix: cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  '*'
+    if Rails.env.production?
+      origins 'https://vue-rails-42c43.web.app'
+    else
+      origins 'http://localhost:8080'
+    end
+
     resource '*', 
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head]
